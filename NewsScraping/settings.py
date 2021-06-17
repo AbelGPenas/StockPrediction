7687cc5e36d71227587fe148d5912d6dd2dbcfb3
AbelGPenas <82783391+AbelGPenas@users.noreply.github.com>
2021-06-17 16:16:56 +0200

    Update setting.py
    Update bot name and spider module

--- NewsScraping/settings.py
+++ NewsScraping/settings.py
@@ -7,10 +7,10 @@
 #     https://docs.scrapy.org/en/latest/topics/downloader-middleware.html
 #     https://docs.scrapy.org/en/latest/topics/spider-middleware.html
 
-BOT_NAME = 'bloomberg_scraper'
+BOT_NAME = 'Marketwatch'
 
-SPIDER_MODULES = ['bloomberg_scraper.spiders']
-NEWSPIDER_MODULE = 'bloomberg_scraper.spiders'
+SPIDER_MODULES = ['NewsScraping.spiders']
+NEWSPIDER_MODULE = 'NewsScraping.spiders'
 
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
